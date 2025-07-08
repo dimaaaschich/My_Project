8fcc756f421062851c21ae35903bde2ee809d2a1
Dmitriy Shishko <dimaaaschich@gmail.com>
2025-07-08 22:08:52 +0300

    создание загрузчика

---
+++ loader.py
@@ -0,0 +1,33 @@
+import csv
+
+from db import db_session
+from models import Deals
+
+def read_csv(file_name ):
+    with open(file_name, 'r') as f:
+        fields = [
+                'agent', 'contractor', 'ds_create_date', 'ds_name',
+                'deal_create_date', 'name_deal', 'deal_subject',
+                'ds_deal_subject', 'price'
+        ]
+        reader = csv.DictReader(f, fields, delimiter=';')
+        for row in reader:
+            save_deals_data(row)
+
+def save_deals_data(row):
+    deal = Deals(
+        agent=row['agent'],
+        contractor=row['contractor'],
+        ds_create_date=row['ds_create_date'],
+        ds_name=row['ds_name'],
+        deal_create_date=row['deal_create_date'],
+        name_deal=row['name_deal'],
+        deal_subject=row['deal_subject'],
+        ds_deal_subject=row['ds_deal_subject'],
+        price=row['price']
+    )
+    db_session.add(deal)
+    db_session.commit()
+
+if __name__ == '__main__':
+    read_csv('deals_all.csv')
--- models.py
+++ models.py
@@ -1,13 +1,20 @@
+from sqlalchemy import Date
 from sqlalchemy.orm import Mapped, mapped_column
-from db import engine, Base
 
+from db import db_session, engine, Base
 
 class Deals(Base):
     __tablename__ = 'deals_list'
     id: Mapped[int] = mapped_column(primary_key=True)
-    agent: Mapped[str] =
-    contractor: Mapped[int]
-    create_date: Mapped[int]
-    number_deal: Mapped[int]
-    subject: Mapped[int]
-    price: Mapped[int]
+    agent: Mapped[str]
+    contractor: Mapped[ str]
+    ds_create_date: Mapped[Date] = mapped_column(Date)
+    ds_name: Mapped[str]
+    deal_create_date: Mapped[Date] = mapped_column(Date)
+    name_deal: Mapped[str]
+    deal_subject: Mapped[str]
+    ds_deal_subject: Mapped[str]
+    price: Mapped[float]
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
